f3142c2eb379e7b22596b048bdbde7bd91247cf9
Lucas Ferreira Teodoro <lucasfteo@gmail.com>
2022-02-26 02:09:04 -0300

    Atualização do código
    Criei todas as funções e fiz as integrações com o banco de dados do sql server para poder fazer consultas e cadastro de usuários diretamente no banco de dados.

--- main.py
+++ main.py
@@ -1,18 +1,110 @@
 import pyodbc
 
-data_conn = (
-    "Driver={SQL Server};"
-    "Server=DESKTOP-G90E8IG\SQLSERVER;"
-    "Database=all_data;"
-    "Trusted_Connection=Yes"
-)
 
-conn = pyodbc.connect(data_conn)
+def connector():
+    data_conn = (
+        "Driver={SQL Server};"
+        "Server=DESKTOP-G90E8IG\SQLSERVER;"
+        "Database=all_data;"
+        "Trusted_Connection=Yes"
+    )
 
-cursor = conn.cursor()
+    conn = pyodbc.connect(data_conn)
 
-command = """INSERT INTO usuarios(nome, nick, senha)
-VALUES ('Ana Maria', 'anam', '456')"""
+    cursor = conn.cursor()
+    return cursor
 
-cursor.execute(command)
-cursor.commit()
+
+def register():
+    cursor = connector()
+    usuario = dict()
+    usuario['nome'] = input('Digite seu nome: ')
+    usuario['nick'] = input('Digite seu nick: ')
+
+    registered = verifier(usuario['nick'])
+
+    if registered:
+        print('Usuário já cadastrado. ')
+        return
+
+    usuario['senha'] = input('Digite sua senha: ')
+
+    command = f"""INSERT INTO usuarios(nome, nick, senha)
+    VALUES ('{usuario["nome"]}', '{usuario["nick"]}', '{usuario["senha"]}')"""
+
+    cursor.execute(command)
+    cursor.commit()
+
+
+def verifier(user):
+    registered = False
+    cursor = connector()
+    string_sql = """
+            SELECT * FROM usuarios;
+        """
+    cursor.execute(string_sql)
+    lines = cursor.fetchall()
+
+    for line in lines:
+
+        if user == line[1]:
+            registered = True
+            break
+
+    return registered
+
+
+def login():
+    cursor = connector()
+
+    string_sql = """
+        SELECT * FROM usuarios;
+    """
+    cursor.execute(string_sql)
+    lines = cursor.fetchall()
+
+    logged = {}
+    online = False
+    nick = input('Digite seu nick: ')
+
+    for line in lines:
+
+        if nick == line[1]:
+            logged['nome'] = line[0]
+            logged['nick'] = line[1]
+            logged['senha'] = line[2]
+            online = True
+            break
+
+    if not online:
+        print('Usuário não cadastrado. ')
+        return
+
+    senha = input('Digite sua senha: ')
+
+    if senha != logged['senha']:
+        print('Usuário ou senha incorreto. ')
+        return
+
+    print(f'Bem vindo(a) {logged["nome"]}')
+
+
+def main():
+    opcao = int(input(
+        "Digite a opção:\n* 1 - Login\n* 2 - Cadastro\n* 3 - Sair\n-> "
+    ))
+
+    if opcao == 1:
+        login()
+        main()
+
+    elif opcao == 2:
+        register()
+        main()
+
+    else:
+        print("Saindo!")
+
+
+if __name__ == '__main__':
+    main()
